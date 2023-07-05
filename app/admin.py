0a12730b53bf5102db564e17e7ecb62348a9c46b
LÂN NGUYỄN HỮU THÀNH <nhtlan21@clc.fitus.edu.vn>
2023-07-05 16:56:50 +0700

    Edit admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from app.models import CustomUser, Job_finder, Employer
 
 # Register your models here.
+
+# admin.site.register(CustomUser)
+# admin.site.register(Employer)
+# admin.site.register(Job_finder)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Job_finder)
+class Job_finderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Employer)
+class EmployerAdmin(admin.ModelAdmin):
+    pass
