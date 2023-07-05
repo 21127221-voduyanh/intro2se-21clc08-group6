23786e53d27c3c64a6f24eaaffca5abd0dbd138e
LÂN NGUYỄN HỮU THÀNH <nhtlan21@clc.fitus.edu.vn>
2023-07-05 17:34:17 +0700

    Urls.py back to old version

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('home/', views.home,name = "home"),
     path('',views.base,name ="base"),
     path('register_finder/',views.register_finder,name="register_finder"),
     path('register_company/',views.register_company,name="register_company"),
     path('login/',views.login,name="login"),
-
 ]
