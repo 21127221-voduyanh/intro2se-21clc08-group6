07ce2981397c4f717651cd310aabd3e9f88c53cb
LÂN NGUYỄN HỮU THÀNH <nhtlan21@clc.fitus.edu.vn>
2023-07-05 16:54:04 +0700

    Edit views.py

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from app.models import CustomUser, Employer, Job_finder
 from django.contrib import messages
