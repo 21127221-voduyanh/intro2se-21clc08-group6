85ddd58e5557cb46a3754952821ec8b35b90b209
LÂN NGUYỄN HỮU THÀNH <nhtlan21@clc.fitus.edu.vn>
2023-07-08 12:11:22 +0700

    Edit login.html and Job_finder model (dateofbirth) to complete Job_finder register function

--- app/models.py
+++ app/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+DATE_INPUT_FORMATS = ['%m-%d-%Y']
 
 # Create your models here.
 class Job_finder(models.Model):
     id = 0
     full_name=models.CharField(max_length=100)
     user=models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
-    gender=models.CharField(max_length=10)
+    gender=models.CharField(max_length=100)
     address=models.TextField()
     city=models.TextField()
-    date_of_birth=models.TextField()
+    date_of_birth=models.DateField()
     introduction=models.TextField()
     
 
