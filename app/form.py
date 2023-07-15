3c0c05a5c47683083bfbd3437e1c2230f5b369a0
Ji21127543 <hnquang21@clc.fitus.edu.vn>
2023-07-15 11:07:11 +0700

    update+profile-better UI

--- app/form.py
+++ app/form.py
@@ -4,9 +4,9 @@ from app.models import Job_finder,Employer
 class EUpdateForm(forms.ModelForm):
     class Meta:
         model = Employer
-        fields = ['company_name', 'user', 'address', 'city', 'introduction']
+        fields = ['company_name', 'address', 'city', 'introduction']
 
 class JFUpdateForm(forms.ModelForm):
     class Meta:
         model = Job_finder
-        fields = ['user', 'address', 'city', 'introduction']
+        fields = ['address', 'city', 'introduction']
