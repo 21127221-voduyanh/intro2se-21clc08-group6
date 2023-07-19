ae75ec8e49555dde1445a8f9dfa6bbf5b07d0bc7
LÂN NGUYỄN HỮU THÀNH <nhtlan21@clc.fitus.edu.vn>
2023-07-19 17:49:53 +0700

    Home page become base , remove base URL

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home,name = "home"),
-    path('',views.base,name ="base"),
+    path('', views.home,name = "home"),
+    #path('',views.base,name ="base"),
     path('register_finder/',views.register_finder,name="register_finder"),
     path('register_company/',views.register_company,name="register_company"),
     path('login/',views.login_user,name="login"),
