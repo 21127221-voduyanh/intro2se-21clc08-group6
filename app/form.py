c1d5c8f150f84105701da10c18a4720346b760dc
21127221-voduyanh <vdanh21@clc.fitus.edu.vn>
2023-07-29 06:37:58 +0700

    style cvform

--- app/form.py
+++ app/form.py
@@ -28,3 +28,13 @@ class CVForm(forms.ModelForm):
     class Meta:
         model = CV
         fields = ['introduction', 'experience', 'education', 'interest', 'languages', 'skill', 'mail', 'phone']
+        widgets = {
+            'introduction' : forms.Textarea(attrs={'class':'form-control'}),
+            'experience' : forms.Textarea(attrs={'class':'form-control'}),
+            'education' : forms.Textarea(attrs={'class':'form-control'}),
+            'interest' : forms.Textarea(attrs={'class':'form-control'}),
+            'languages' : forms.Textarea(attrs={'class':'form-control'}),
+            'skill' : forms.Textarea(attrs={'class':'form-control'}),
+            'mail' : forms.EmailInput(attrs={'class':'form-control'}),
+            'phone' : forms.NumberInput(attrs={'class':'form-control'})
+        }
